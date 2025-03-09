d6e89017706787427bade38c6e53184806ac4875
Lankaprabhudeva <prabhudevalanka@gmail.com>
2025-03-09 14:18:00 +0530

    Updated project files

--- src/main/java/Basepackage/Baseclass.java
+++ src/main/java/Basepackage/Baseclass.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
